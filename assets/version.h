bbed2e050ae35020d6cc2613986986f30ba635b8
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2017-02-17 09:11:24 +0200

    Revert "Added sample files for git+cmake version"
    This reverts commit 9e866174dca953ae1739c931ba7c346a112cc038.

--- assets/version.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Version {
-public:
-    Version();
-    const char * const git_sha1;
-    const char * const git_description;
-    const char * const package;
-    const char * const build_type;
-};
